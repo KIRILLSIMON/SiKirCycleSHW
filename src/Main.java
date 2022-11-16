c80322db9044830cfd9ba4ed7a0377afe227cf93
sikir <sikir2022@gmail.com>
2022-11-16 16:33:08 +0300

    Nachalnoye DZ Simonova KO po Lesson1 Cycle Theme.

---
+++ src/Main.java
@@ -0,0 +1,59 @@
+public class Main {
+    public static void main(String[] args) {
+       /*System.out.println("Zadacha1");
+       for(int i=0; i<11;i++){
+               System.out.println("i= "+i);
+            }*/
+       /*System.out.println("Avtorskie interpretacii k Zadache2");
+       for(int i=10;i>=0;i--)
+        System.out.println("10ть доказательно-интерпретационных строк(МАНТР), что Я-НЕ дурак!!!");*/
+        /*System.out.println("Zadacha2");
+        for (int i=10;i>=0;i--){
+           System.out.println("i= "+i);
+                }*/
+        /*System.out.println("Zadacha3");
+        for(int i=0;i<=15;){
+        i+=2;
+        System.out.println("Чётные из вышенаписанного ряда-это такие числа:i= "+i);
+        }*/
+        /*System.out.println("Zadacha4");
+        int m1=2;
+        for(int m2=0;m2<11;m2++){
+            System.out.println("2*m2= "+m1*m2);
+        }System.out.println("Smotri vishe r-e zadach 1y, 2y i 2y zadachi -3ego zadaniya po 2y chasti Uroka cycli");*/
+        /*System.out.println("5Zadacha na naibolshee v ryadu chislo");
+        for(int i=10; i>=-10;i--){
+            System.out.println(i);
+            } System.out.println("Vyshe reshena 4ya zadacha 1`go DZ 1go Uroka po cyclam");*/
+    /*System.out.println("Samostoyatelnaya rabota");
+    int factorial=7;
+    for (int m2=factorial-1;m2>=1;m2--){
+        factorial=factorial*m2;
+    }System.out.println(factorial);*/
+      /*System.out.println("Спис. високосных годов от Симонова КО с 1904г. включительно по 2096 г. содержит:  см.ниже");
+        for(int VG=1904;VG<=2092;){
+            VG+=4;
+        System.out.println(VG+" год-ВИСОКОСНЫЙ!");}*/
+        /*System.out.println("Решение 7рочной задачи из ДЗ");
+        for (int num=7;num<=91;){
+            num+=7;
+            System.out.println(num);}*/
+       /* System.out.println("Задача умножения на 2-до 512 ");
+        for (int m=2;m<=256;){
+            m*=2;
+            System.out.println(m);
+                }*/
+   /* System.out.println("Reshenie 1y zadachi po bankovskomu delu");
+    int zVmInR=29000;
+    for(int timToMonTrans=0;timToMonTrans<=11;){
+    timToMonTrans++;
+    System.out.println("К кон. "+timToMonTrans+ "-го мес. общ.зананачка сейчас: "+zVmInR*timToMonTrans+"руб.0коп");}*/
+
+    }
+}
+
+
+
+
+
+
